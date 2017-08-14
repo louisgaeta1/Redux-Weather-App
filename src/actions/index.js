66c53898b9efbea2f03b4566b7be4ebd2e427858
Lou Gaeta <louis.gaeta1@gmail.com>
2017-08-14 13:29:56 -0400

    50% through weather application

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+const API_KEY = '454ff8d89de690ab7a826a22d62ff9f3';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
